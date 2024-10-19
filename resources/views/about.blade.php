960339b2c4a2ba4bb9efeb88207b94a09cee767d
AHMADAZQIYAUCI <pesp02466@gmail.com>
2024-10-20 02:30:44 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>about</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body>
+    <h1 class="text-success">ini adalah halaman about</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" ></script>
+</body>
+</html>
